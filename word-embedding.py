1986aeb458ec4dc1dc847bacada4e7028e7a724f
aws-ehj <ehongjin@amazon.com>
2025-02-17 04:44:56 +0900

    Update word-embedding.py

--- word-embedding.py
+++ word-embedding.py
@@ -3,23 +3,21 @@
 pip install gensim 
 
 
-#Step 2: Load Pre-trained Word2Vec Model
-#We’ll use the pre-trained Word2Vec model from Google News.
+#Step 2: Load Pre-trained glove-wiki-gigaword-50 Model(비교적 크기가 작은 모델 다운로드)
 
 import gensim.downloader as api model = api.load('glove-wiki-gigaword-50')
 
 #Step 3: Exploring Word Embeddings
 
-#Let’s find the vector for a word and perform some operations.
-# Get vector for a word 
+# Get vector for a word (워드 King에 대한 벡터값을 조회해 봅니다.) 
 word_vector = model['king’] 
 
 
-# Find similar words 
+# Find similar words (King이라는 단어와 비슷한 단어를 5개 찾아봅니다.)
 similar_words = model.most_similar('king', topn=5) 
 print("Similar words to 'king':", similar_words) 
 
 
-# Vector arithmetic 
+# Vector arithmetic (워드에 대한 더하기 뺴기를 해 봅니다.)
 result = model.most_similar(positive=['king', 'woman'], negative=['man'], topn=1) 
 print("Result of 'king' - 'man' + 'woman':", result)
